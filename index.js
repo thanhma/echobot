9689f3822ac58f4ae13ef2e56bdbecf4ed8178ec
Thanh Dang Tuan <thanhma.san@gmail.com>
2017-12-01 15:07:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,43 @@
-'use strict';
+'use strict'
+const line = require('node-line-bot-api')
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
 
-const line = require('@line/bot-sdk');
-const express = require('express');
-
-// create LINE SDK config from env variables
-const config = {
-  channelAccessToken: '/D/ka4Qj/JmwAKHoH1VXlJij+RnAYAI31LS8ogiSXejzyXS1mAreGzlV07mo/5N0X6LJSj9VyHDnsJfO9km3K6yJvkudaQeX2gXcLHpI1qNps9QLuw8ogoRqIWH5/qsDV3o+kDj800YLw98M01L40AdB04t89/1O/w1cDnyilFU=',
-  channelSecret: '91d571d2bb897d6779eb7329d96dd1ec',
-};
-
-// create LINE SDK client
-const client = new line.Client(config);
-
-// create Express app
-// about Express itself: https://expressjs.com/
-const app = express();
-
-// register a webhook handler with middleware
-// about the middleware, please refer to doc
-app.post('/webhook', line.middleware(config), (req, res) => {
-  Promise
-    .all(req.body.events.map(handleEvent))
-    .then((result) => res.json(result));
-});
-
-// event handler
-function handleEvent(event) {
-  if (event.type !== 'message' || event.message.type !== 'text') {
-    // ignore non-text-message event
-    return Promise.resolve(null);
+// need raw buffer for signature validation
+app.use(bodyParser.json({
+  verify (req, res, buf) {
+    req.rawBody = buf
   }
+}))
 
-  // create a echoing text message
-  const echo = { type: 'text', text: event.message.text };
+// init with auth
+line.init({
+  accessToken: '/D/ka4Qj/JmwAKHoH1VXlJij+RnAYAI31LS8ogiSXejzyXS1mAreGzlV07mo/5N0X6LJSj9VyHDnsJfO9km3K6yJvkudaQeX2gXcLHpI1qNps9QLuw8ogoRqIWH5/qsDV3o+kDj800YLw98M01L40AdB04t89/1O/w1cDnyilFU=',
+  // (Optional) for webhook signature validation
+  channelSecret: '91d571d2bb897d6779eb7329d96dd1ec'
+})
 
-  // use reply API
-  return client.replyMessage(event.replyToken, echo);
-}
+app.post('/webhook/', line.validator.validateSignature(), (req, res, next) => {
+  // get content from request body
+  const promises = req.body.events.map(event => {
+    // reply message
+    return line.client
+      .replyMessage({
+        replyToken: event.replyToken,
+        messages: [
+          {
+            type: 'text',
+            text: event.message.text
+          }
+        ]
+      })
+  })
+  Promise
+    .all(promises)
+    .then(() => res.json({success: true}))
+})
 
-// listen on port
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`listening on ${port}`);
-});
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Example app listening on port 3000!')
+})
