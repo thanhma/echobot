5e08cc0ea8d919ddc0c6bdef878e7371e99e2e29
Thanh Dang Tuan <thanhma.san@gmail.com>
2017-12-01 16:17:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ const app = express();
 // register a webhook handler with middleware
 // about the middleware, please refer to doc
 app.post('/webhook', line.middleware(config), (req, res) => {
-  res.send('Got a POST request');
+  Promise
+    .all(req.body.events.map(handleEvent))
+    ;
 });
 
 // event handler
