4f32dba9ebbf0f34d9daf5b57910557d7bd6a521
Thanh <vij00002@partner.nri.co.jp>
2017-12-01 16:22:07 +0700

    Test

--- index.js
+++ index.js
@@ -26,16 +26,7 @@ app.post('/webhook', line.middleware(config), (req, res) => {
 
 // event handler
 function handleEvent(event) {
-  if (event.type !== 'message' || event.message.type !== 'text') {
-    // ignore non-text-message event
-    return Promise.resolve(null);
-  }
-
-  // create a echoing text message
-  const echo = { type: 'text', text: event.message.text };
-
-  // use reply API
-  return client.replyMessage(event.replyToken, echo);
+ 
 }
 
 // listen on port
