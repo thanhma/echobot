5c35cdfc112f26594eeb8579085123e2b0bf7113
Thanh <vij00002@partner.nri.co.jp>
2017-12-01 15:31:11 +0700

    Fix

--- index.js
+++ index.js
@@ -1,43 +1,45 @@
-'use strict'
-const line = require('node-line-bot-api')
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
+'use strict';
 
-// need raw buffer for signature validation
-app.use(bodyParser.json({
-  verify (req, res, buf) {
-    req.rawBody = buf
-  }
-}))
+const line = require('@line/bot-sdk');
+const express = require('express');
 
-// init with auth
-line.init({
-  accessToken: '/D/ka4Qj/JmwAKHoH1VXlJij+RnAYAI31LS8ogiSXejzyXS1mAreGzlV07mo/5N0X6LJSj9VyHDnsJfO9km3K6yJvkudaQeX2gXcLHpI1qNps9QLuw8ogoRqIWH5/qsDV3o+kDj800YLw98M01L40AdB04t89/1O/w1cDnyilFU=',
-  // (Optional) for webhook signature validation
-  channelSecret: '91d571d2bb897d6779eb7329d96dd1ec'
-})
+// create LINE SDK config from env variables
+const config = {
+  channelAccessToken: '/D/ka4Qj/JmwAKHoH1VXlJij+RnAYAI31LS8ogiSXejzyXS1mAreGzlV07mo/5N0X6LJSj9VyHDnsJfO9km3K6yJvkudaQeX2gXcLHpI1qNps9QLuw8ogoRqIWH5/qsDV3o+kDj800YLw98M01L40AdB04t89/1O/w1cDnyilFU=',
+  channelSecret: '91d571d2bb897d6779eb7329d96dd1ec',
+};
 
-app.post('/webhook/', line.validator.validateSignature(), (req, res, next) => {
-  // get content from request body
-  const promises = req.body.events.map(event => {
-    // reply message
-    return line.client
-      .replyMessage({
-        replyToken: event.replyToken,
-        messages: [
-          {
-            type: 'text',
-            text: event.message.text
-          }
-        ]
-      })
-  })
+// create LINE SDK client
+const client = new line.Client(config);
+
+// create Express app
+// about Express itself: https://expressjs.com/
+const app = express();
+
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
+app.post('/webhook', line.middleware(config), (req, res) => {
   Promise
-    .all(promises)
-    .then(() => res.json({success: true}))
-})
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result));
+});
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Example app listening on port 3000!')
-})
+// event handler
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    // ignore non-text-message event
+    return Promise.resolve(null);
+  }
+
+  // create a echoing text message
+  const echo = { type: 'text', text: event.message.text };
+
+  // use reply API
+  return client.replyMessage(event.replyToken, echo);
+}
+
+// listen on port
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
